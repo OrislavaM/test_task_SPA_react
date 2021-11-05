a8d859248450b89fc176477b7604e93303ee4760
OrislavaM <orisyamatvienko@gmail.com>
2021-11-05 14:34:43 +0200

    adding cards to the product page, use API

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CardList from "../components/UI/Card/Card";
+import "../Styles/Products.css";
 
 function Products() {
-    return <div>Products is work!</div>;
+    const [cards, setCards] = useState([]);
+
+    const getCardRequest = async () => {
+        const url = "https://api.thedogapi.com/v1/breeds?limit=8&page=0";
+
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        console.log(responseJson);
+        setCards(responseJson);
+    };
+
+    useEffect(() => {
+        getCardRequest();
+    }, []);
+
+    return (
+        <div className="container">
+            <h3>Product Page</h3>
+            <div className="products">
+                <CardList cards={cards} />
+            </div>
+        </div>
+    );
 }
 
 export default Products;
---
+++ src/components/UI/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../Card/Card.css";
+
+const CardList = (props) => {
+    return (
+        <>
+            {props.cards.map((card) => (
+                <div className="dogs_card">
+                    <img src={card.image.url} alt="dogs"></img>
+                    <div className="dogs_information">
+                        <p className="dogs_name">{card.name}</p>
+                        <p className="dogs_bred">{card.bred_for}</p>
+                        <p className="dogs_temperament">{card.temperament}</p>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
+export default CardList;
